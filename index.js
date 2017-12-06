902f2c016607b01a08f2c81e6820cd5fc13ea50c
lihxhit <419948733@qq.com>
2017-12-06 17:36:03 +0800

    up

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+
+    book:{
+        assets:'./assets',
+        js:[
+            'index.js'
+        ],
+        css:[],
+        templates:{}
+    },
+    // Map of hooks
+    hooks: {},
+
+    // Map of new blocks
+    blocks: {},
+
+    // Map of new filters
+    filters: {}
+};
